b7a1459e172ecd68cd8ba0b37eae8483beaecf22
Dercio Pale <31033591+derciopale@users.noreply.github.com>
2021-05-17 13:46:23 +0200

    Fundo Principal

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -4,7 +4,27 @@ class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    
-    return Container();
+    Widget _buildBodyBack() => Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 0, 6, 26),
+            Color.fromARGB(255,0, 26, 102)
+            
+            ],
+            // Onde comeca a cor e termina
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+            )
+        ),
+      );
+      
+    // Stack para colocar qualquer coisa em cima da tela
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack()
+      ],
+
+    );
   }
 }
